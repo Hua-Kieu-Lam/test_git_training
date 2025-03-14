05a5c700ec39c9bff1b79d1bf92e885214dcc0bf
Hua Kieu Lam <157774509+Hua-Kieu-Lam@users.noreply.github.com>
2025-03-14 11:31:34 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("In a nutshell, generics enable types (classes and interfaces) to be parameters when defining classes, interfaces and methods.");
 
         System.out.println("Much like the more familiar formal parameters used in method declarations, type parameters provide a way for you to re-use the same code with different inputs. The difference is that the inputs to formal parameters are values, while the inputs to type parameters are types.");
+
+        int num = 10;
     }
 }
