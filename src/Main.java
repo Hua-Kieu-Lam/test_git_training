8a58d2392f8d7a6f5d2f44ce6938c358e5f781c4
Lam Kiều <huakieulam@gmail.com>
2025-03-14 11:03:46 +0700

    push base code

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Generics");
+        System.out.println("In a nutshell, generics enable types (classes and interfaces) to be parameters when defining classes, interfaces and methods.");
+    }
+}
